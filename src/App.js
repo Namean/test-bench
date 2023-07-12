66a41e9d51147c87f1b8b28679d845532377d764
Namean <dwilsoncnc@gmail.com>
2023-07-12 02:20:43 +0000

    made a element that changes every second

--- src/App.js
+++ src/App.js
@@ -4,11 +4,39 @@ import './styles/App.css';
 import TabContainer from "./components/TabContainer.component";
 
 import Accordion from './react.dev/sharing-state-between-components/Accordion.component';
+import { useState, useEffect } from 'react';
 
- 
+
+
+
+const Hello = () => {
+  let red = { color: 'red' }
+  let green = { color: 'green' }
+  const [state, setState] = useState(0);
+
+  useEffect(() => {
+      setInterval(() => {
+          if (state === 0) {
+            setState(1);
+          } else if (state === 1) {
+            setState(0);
+          }
+      }, 1000)   
+  }, [state, setState])
+
+  return (
+    <>
+      <h1>The current state is {state}</h1>
+    </>
+  )
+}
+
 const App = () => {
   return (
-	 <TabContainer />
+    <>
+      <Hello />
+      <TabContainer />
+    </>
   );
 }
 
